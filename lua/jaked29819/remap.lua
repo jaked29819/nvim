ff5baec828ce688c4b12d14311098312b3bffda6
Jake Dorer <jake@deltathermalinc.com>
2024-12-04 20:03:31 -0700

    update to move to server

--- lua/jaked29819/remap.lua
+++ lua/jaked29819/remap.lua
@@ -1,3 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("n", "<leader>fw", function()
+    vim.fn.setreg("/", "\\<" .. vim.fn.expand("<cword>") .. "\\>")
+    vim.opt.hlsearch = true
+end, { noremap = true, silent = true})
+
